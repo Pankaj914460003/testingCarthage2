6a34f1efa570fd1df1cf94c8d4f7e0949fe72679
Iziss_Technology <iziss_technology@Iziss-Technologys-Mac-mini.local>
2017-06-20 19:16:44 +0530

    first

---
+++ testingCarthage2/testing.swift
@@ -0,0 +1,18 @@
+//
+//  testing.swift
+//  testingCarthage
+//
+//  Created by Iziss_Technology on 20/06/17.
+//  Copyright © 2017 Pankaj Ramchandani & Co. All rights reserved.
+//
+
+import UIKit
+
+public class testing: UIView {
+
+    public func tst()
+    {
+        print("Working")
+    }
+    
+}
