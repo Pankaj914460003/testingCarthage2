b2db5f7f9674ac0b8aeadb5321df34998e74dd07
Iziss_Technology <iziss_technology@Iziss-Technologys-Mac-mini.local>
2017-06-20 19:15:15 +0530

    Initial Commit

---
+++ testingCarthage2/testingCarthage2.h
@@ -0,0 +1,19 @@
+//
+//  testingCarthage2.h
+//  testingCarthage2
+//
+//  Created by Iziss_Technology on 20/06/17.
+//  Copyright © 2017 Pankaj Ramchandani & Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testingCarthage2.
+FOUNDATION_EXPORT double testingCarthage2VersionNumber;
+
+//! Project version string for testingCarthage2.
+FOUNDATION_EXPORT const unsigned char testingCarthage2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testingCarthage2/PublicHeader.h>
+
+
